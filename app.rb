c2ec1e0a82a24530c857ce942e47667ed673c38f
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-07-21 00:41:13 +0800

    Add models for other tables

--- app.rb
+++ app.rb
@@ -2,8 +2,35 @@ class RedmartSinatra < Sinatra::Base
   get '/' do
     erb 'Hello World'
   end
-  get '/Profile' do
-    @users = User.all
-    erb :'userView'
+  get '/users' do
+    @headers = User.column_names
+    @items = User.all
+    erb :'tableView'
+  end
+  get '/products' do
+    @headers = Product.column_names
+    @items = Product.all
+    erb :'tableView'
+  end
+  get '/orders' do
+    # @headers = Order.column_names
+    # @items = Order.all
+    # erb :'tableView'
+    erb 'This table has not been created yet.'
+  end
+  get '/purchases' do
+    @headers = Purchase.column_names
+    @items = Purchase.all
+    erb :'tableView'
+  end
+  get '/categories' do
+    @headers = Category.column_names
+    @items = Category.all
+    erb :'tableView'
+  end
+  get '/brands' do
+    @headers = Brand.column_names
+    @items = Brand.all
+    erb :'tableView'
   end
 end
--- config.ru
+++ config.ru
@@ -4,6 +4,12 @@ Bundler.require
 
 #for models
 require './models/user'
+require './models/product'
+require './models/order'
+require './models/purchase'
+require './models/category'
+require './models/brand'
+
 #for controller
 require './app'
 

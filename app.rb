36cfd4f464eb56b76582d1083bea1a84e3747a2f
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-07-21 09:17:34 +0800

    Update redirection code

--- app.rb
+++ app.rb
@@ -2,35 +2,26 @@ class RedmartSinatra < Sinatra::Base
   get '/' do
     erb 'Hello World'
   end
-  get '/users' do
-    @headers = User.column_names
-    @items = User.all
-    erb :'tableView'
-  end
-  get '/products' do
-    @headers = Product.column_names
-    @items = Product.all
-    erb :'tableView'
-  end
-  get '/orders' do
-    # @headers = Order.column_names
-    # @items = Order.all
-    # erb :'tableView'
-    erb 'This table has not been created yet.'
-  end
-  get '/purchases' do
-    @headers = Purchase.column_names
-    @items = Purchase.all
-    erb :'tableView'
-  end
-  get '/categories' do
-    @headers = Category.column_names
-    @items = Category.all
-    erb :'tableView'
-  end
-  get '/brands' do
-    @headers = Brand.column_names
-    @items = Brand.all
+  get '/tables/:id' do
+    if(params[:id] == 'users')
+      @headers = User.column_names
+      @items = User.all
+    elsif (params[:id] == 'products')
+      @headers = Product.column_names
+      @items = Product.all
+    elsif(params[:id] == 'orders')
+      @headers = Order.column_names
+      @items = Order.all
+    elsif(params[:id] == 'purchases')
+      @headers = Purchase.column_names
+      @items = Purchase.all
+    elsif(params[:id] == 'categories')
+      @headers = Category.column_names
+      @items = Category.all
+    elsif(params[:id] == 'brands')
+      @headers = Brand.column_names
+      @items = Brand.all
+    end
     erb :'tableView'
   end
 end

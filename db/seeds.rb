d50953043c3dc0e423e7cbd812e1bd52b16ea0f5
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-07-24 21:31:37 +0800

    seed file fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,22 +4,23 @@ require './models/brand'
 require './models/product'
 require 'faker'
 
-For creating dummy user data
+# For creating dummy user data
 (0..5).each do |_x|
   User.create(name: Faker::StarWars.character, email: Faker::Internet.email, password: Faker::Internet.password, cc_number: Faker::Business.credit_card_number)
 end
 
-For creating dummy category data
+# For creating dummy category data
 @categoryList = ['New', 'Marketplace', 'Fresh Produce', 'Meat & Seafood', 'Dairy & Chilled', 'Bakery', 'Frozen', 'Beverages', 'Food Cupboard', 'Alcohol', 'Health & Beauty', 'Household & Pet', 'Baby & Child', 'Home & Outdoor']
 
 @categoryList.each do |category|
   Category.create(category_name: category)
 end
 
-For creating dummy brand data
+# For creating dummy brand data
 (0..30).each do |_item|
   Brand.create(brand_name: Faker::Company.name)
 end
+
 brand_count = Brand.all.count
 category_count = Category.all.count
 
